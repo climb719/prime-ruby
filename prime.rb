692abcda1530cf3d43b1324910e3da5ca7fdbd72
climb719 <mary.oldweiler@gmail.com>
2021-01-18 15:27:39 -0500

    commit tree ea23286b1b6b61f2c986d4677c4a864ac7e58a4f

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+require 'prime'
+
+def prime?(num)
+    range = (2..num-1).to_a
+    if num > 1
+        range.none? do |i|
+         num % i == 0
+    end
+else false
+end
+end
+
